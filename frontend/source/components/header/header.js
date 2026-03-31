dba8fc7895ce297c196cef24033f5df5acb762a5
Артём Цыганов <tsiganovartem06@gmail.com>
2026-03-31 17:17:10 +0500

    home page preview

--- frontend/source/components/header/header.js
+++ frontend/source/components/header/header.js
@@ -1,12 +1,19 @@
-
-
 export function headerTemplate() {
     const header = 
-    '<div class="header-placeholder">\
-        <div>Каталог</div>\
-        <div>Статьи</div>\
-        <img src="assets/logo.png" alt="logo">\
-        <div> icon </div>\
-    </div>'
+    `
+    <link rel="stylesheet" href="source/components/header/header.css">
+    <div class="header-containter">
+        <div class="header-group left">
+            <div class="header-item">Каталог</div>
+            <div class="header-item">Статьи</div>
+        </div>
+        <img src="assets/logo.png" alt="logo">
+        <div class="header-group right">
+            <div class="header-item"> icon </div>
+        </div>
+    </div>
+    `
+
     return header;
 }
+
