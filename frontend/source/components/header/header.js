543942afd90cf717d116d6448773516139b0cc8e
Артём Цыганов <tsiganovartem06@gmail.com>
2026-03-31 16:05:18 +0500

    init repository

---
+++ frontend/source/components/header/header.js
@@ -0,0 +1,12 @@
+
+
+export function headerTemplate() {
+    const header = 
+    '<div class="header-placeholder">\
+        <div>Каталог</div>\
+        <div>Статьи</div>\
+        <img src="assets/logo.png" alt="logo">\
+        <div> icon </div>\
+    </div>'
+    return header;
+}
---
+++ frontend/source/main.js
@@ -0,0 +1,2 @@
+import { headerTemplate } from './components/header/header.js';
+document.body.insertAdjacentHTML('afterbegin', headerTemplate());
